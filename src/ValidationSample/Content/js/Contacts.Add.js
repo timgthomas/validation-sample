c2b1aac3debaa79b51fdf2fc266ffbd21d0807a2
Tim G. Thomas <tim@timgthomas.com>
2013-12-20 18:34:22 -0600

    implement client-side validation

---
+++ src/ValidationSample/Content/js/Contacts.Add.js
@@ -0,0 +1,36 @@
+﻿$('form').on('submit', function() {
+
+    var request,
+        data = $(this).serialize();
+
+    var request = $.ajax({
+        type: 'post',
+        data: data
+    });
+
+    request.fail(function(xhr) {
+        var response,
+            properties, property,
+            i;
+
+        if (xhr.status === 400) {
+            response = JSON.parse(xhr.responseText);
+            var fields = Object.keys(response);
+
+            fields.forEach(function(field) {
+                if (response[field].Errors && response[field].Errors.length > 0) {
+                    $('[name=' + field + ']')
+                        .closest('p')
+                        .addClass('invalid');
+                }
+            });
+        }
+    });
+
+    request.success(function() {
+
+    });
+
+    return false;
+
+});
